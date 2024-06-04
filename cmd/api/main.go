913ffa8a9fd3e69fdb5a278f9a4e41bec4f73782
MCarbono <marcelo.carbono@tino.com.br>
2024-06-04 15:59:31 -0300

    logs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,11 +10,13 @@ import (
 func main() {
 	// how to test it? curl http://localhost:4000/flamengo
 	http.HandleFunc("/flamengo", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Log de teste deploy")
 		fmt.Fprintf(w, "aqui é vasco porra")
 	})
 
 	//create a health check endpoint
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Log de teste deploy")
 		fmt.Fprintf(w, "I'm alive!")
 		w.WriteHeader(http.StatusOK)
 	})
@@ -29,7 +31,7 @@ func main() {
 		Addr:    fmt.Sprintf(":%d", intPort),
 		Handler: nil,
 	}
-
+	fmt.Fprintf(w, "Log de teste deploy")
 	fmt.Printf("Server is running on port %d\n", intPort)
 	srv.ListenAndServe()
 }
