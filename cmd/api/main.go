cc925bd15e9ee372c0cdf614a0e1d67d31e3b8bd
perebaj <perebaj@gmail.com>
2024-06-05 12:05:02 -0300

    :sparkles: alive2

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -37,7 +37,7 @@ func main() {
 
 	//create a health check endpoint
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "I'm alive!")
+		fmt.Fprintf(w, "I'm alive 2!")
 		w.WriteHeader(http.StatusOK)
 	})
 
